0e126e411032232c29551d917825ced77cdb0071
Lizzie <leejihye7117@gmail.com>
2022-09-20 17:51:02 +0900

    Login Controller email

--- project/src/components/Login.js
+++ project/src/components/Login.js
@@ -1,7 +1,6 @@
 import React from "react";
 import TextField from "@mui/material/TextField";
 import Checkbox from "@mui/material/Checkbox";
-import Button from "@mui/material/Button";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
@@ -9,68 +8,129 @@ import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Avatar from "@mui/material/Avatar";
+import { useForm, Controller } from "react-hook-form";
+import LoadingButton from "@mui/lab/LoadingButton";
+import Api from "../api/Api";
 
-function Login() {
+function sleep(delay) {
+  return new Promise((resolve) => setTimeout(resolve, delay));
+}
+
+export default function Login() {
+  const {
+    control,
+    handleSubmit,
+    setError,
+    clearErrors,
+    formState: { isSubmitting },
+  } = useForm();
+
+  /**
+   * 로그인 실행 함수
+   */
+  const onSubmit = handleSubmit(async (values) => {
+    // 호출 시작
+
+    try {
+      const { data } = await Api.login({
+        email: values.email,
+        password: "1234",
+      });
+
+      // TODO: 성공시
+      clearErrors();
+      alert("로그인이 완료되었습니다!");
+    } catch (e) {
+      // TODO: 실패시
+      alert("로그인에 실패하였습니다.");
+
+      // 아이디랑, 비밀번호가 다름
+      // setError("email", {
+      //   type: "required",
+      //   message: "아이디 또는 비밀번호가 달라요!",
+      // });
+      return;
+    }
+  });
+
   return (
     <Container component="main" maxWidth="sm">
-      <Box
-        sx={{
-          marginTop: 8,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <Avatar
-          src="https://avatars.dicebear.com/api/big-smile/:seed.svg"
-          sx={{ width: 70, height: 70 }}
-        ></Avatar>
-        <Typography component="h1" variant="h5">
-          어서와서 당신의 동물을 자랑해주세요 !
-        </Typography>
-
-        <TextField
-          label="Email Address"
-          name="email"
-          autoComplete="email"
-          margin="normal"
-          required
-          fullWidth
-          autoFocus
-        />
-        <TextField
-          label="Password"
-          name="password"
-          type="password"
-          autoComplete="current-password"
-          margin="normal"
-          required
-          fullWidth
-        />
-        <FormControlLabel
-          control={<Checkbox value="remember" color="primary" />}
-          label="당신을 기억할게요 :)"
-        />
-        <Button
-          variant="contained"
-          type="submit"
-          fullWidth
-          sx={{ mt: 3, mb: 2 }}
+      <form onSubmit={onSubmit}>
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
         >
-          Sign in
-        </Button>
+          <Avatar
+            src="https://avatars.dicebear.com/api/big-smile/:seed.svg"
+            sx={{ width: 70, height: 70 }}
+          />
+          <Typography component="h1" variant="h5">
+            어서와서 당신의 동물을 자랑해주세요 !
+          </Typography>
 
-        <Grid container>
-          <Grid item xs>
-            <Link>비밀번호를 잊으셨나요?</Link>
+          <Controller
+            control={control}
+            name="email"
+            defaultValue=""
+            rules={{
+              required: {
+                value: true,
+                message: "이메일을 입력해주세요!",
+              },
+            }}
+            render={({ field, fieldState: { error } }) => {
+              return (
+                <TextField
+                  label="Email Address"
+                  autoComplete="email"
+                  margin="normal"
+                  fullWidth
+                  autoFocus
+                  {...field}
+                  error={!!error?.message}
+                  helperText={error?.message}
+                />
+              );
+            }}
+          />
+
+          <TextField
+            label="Password"
+            name="password"
+            type="password"
+            autoComplete="current-password"
+            margin="normal"
+            fullWidth
+            helperText="비밀번호를 알려주세요!"
+            error
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="당신을 기억할게요 :)"
+          />
+          <LoadingButton
+            loading={isSubmitting}
+            variant="contained"
+            type="submit"
+            fullWidth
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign in
+          </LoadingButton>
+          <Grid container>
+            <Grid item xs>
+              <Link>비밀번호를 잊으셨나요?</Link>
+            </Grid>
+            <Grid item>
+              <Link>처음이시군요!</Link>
+            </Grid>
           </Grid>
-          <Grid item>
-            <Link>처음이시군요!</Link>
-          </Grid>
-        </Grid>
-      </Box>
+        </Box>
+      </form>
     </Container>
   );
 }
-
-export default Login;
