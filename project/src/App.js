b7a4149a0fac1cf5866abdf3c7292b134a98b212
Lizzie <leejihye7117@gmail.com>
2022-09-22 01:19:45 +0900

    Router

--- project/src/App.js
+++ project/src/App.js
@@ -2,14 +2,7 @@ import React from "react";
 import "./App.css";
 import ThemeProvider from "@mui/material/styles/ThemeProvider";
 import theme from "./theme";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Routes,
-  Link,
-  BrowserRouter,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
@@ -17,14 +10,12 @@ import SignUp from "./components/SignUp";
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Login />}></Route>
-            <Route path="/SignUp/" element={<SignUp />}></Route>
-          </Routes>
-        </BrowserRouter>
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />}></Route>
+          <Route path="/SignUp/" element={<SignUp />}></Route>
+        </Routes>
+      </Router>
     </ThemeProvider>
   );
 }
